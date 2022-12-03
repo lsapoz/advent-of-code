f0a689f5681f7df4a90a192e2a856ccb05afd667
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2022-12-02 17:29:07 -0800

    Day 2 Part 2

--- 2022/02.py
+++ 2022/02.py
@@ -6,6 +6,10 @@ lines = Path(here/"02.txt").read_text().splitlines()
 # 0 = draw, 1 = win, 2 = loss
 outcome_points = {0: 3, 1: 6, 2: 0} 
 
+# Part 1
+# A = X = Rock = 1 point
+# B = Y = Paper = 2 points
+# C = Z = Scissors = 3 points
 my_total_score = 0
 for line in lines:
     moves = line.split()
@@ -13,4 +17,22 @@ for line in lines:
     my_move = ord(moves[1]) - ord('X') + 1
     outcome = (my_move - elf_move) % 3
     my_total_score += my_move + outcome_points[outcome]
-print(f"My Total Score: {my_total_score}")
+print(f"Part 1 Score: {my_total_score}")
+
+# Part 2
+# X = Lose
+# Y = Draw
+# Z = Win
+
+# Map letters to the numbers we originally associated with each outcome
+outcome_map = {'X': 2, 'Y': 0, 'Z': 1}
+
+my_total_score = 0
+for line in lines:
+    split_line = line.split()
+    elf_move = ord(split_line[0]) - ord('A') + 1
+    outcome = outcome_map[split_line[1]]
+    my_move = elf_move + outcome 
+    my_move = my_move % 3 if my_move > 3 else my_move
+    my_total_score += my_move + outcome_points[outcome]
+print(f"Part 2 Score: {my_total_score}")
