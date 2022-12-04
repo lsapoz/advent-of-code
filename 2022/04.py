418469379fd2e6be85b66c25deae3d508478de1a
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2022-12-04 12:41:17 -0800

    Day 4 Part 2

--- 2022/04.py
+++ 2022/04.py
@@ -10,9 +10,20 @@ def is_fully_contained(a, b):
     return b1 >= a1 and b2 <= a2
 
 
+# do ranges a and b overlap
+def is_overlapping(a, b):
+    a1, a2 = [int(x) for x in a.split('-')]
+    b1, b2 = [int(x) for x in b.split('-')]
+    return a1 <= b2 and b1 <= a2
+
+
 num_fully_contained = 0
+num_overlapping = 0
 for line in lines:
     range1, range2 = line.split(',')
-    if is_fully_contained(range1, range2) or is_fully_contained(range2, range1):
-        num_fully_contained += 1
+    if is_overlapping(range1, range2):
+        num_overlapping += 1
+        if is_fully_contained(range1, range2) or is_fully_contained(range2, range1):
+            num_fully_contained += 1
 print(f"Num fully contained: {num_fully_contained}")
+print(f"Num overlapping: {num_overlapping}")
