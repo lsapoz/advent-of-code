3f12fca0f5e5e647ee83dedd4dc62f5b6cf59473
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2022-12-05 17:47:42 -0800

    Day 5 Part 2

--- 2022/05.py
+++ 2022/05.py
@@ -16,23 +16,31 @@ for i, line in enumerate(lines):
         break
 
 num_stacks = int(stack_data.pop().split()[-1])
-stacks = [[] for _ in range(num_stacks)]
+initial_stacks = [[] for _ in range(num_stacks)]
 while stack_data:
     line = stack_data.pop()
     line_idx = 1  # first character on a stack line is a '[', so skip it
     for i in range(num_stacks):
         letter = line[line_idx]
         if letter.strip():
-            stacks[i].append(letter)
+            initial_stacks[i].append(letter)
         line_idx += 4  # after every letter we have '] [', skip straight to the next letter
 
+# Part 1 - CrateMover 9000 - one crate moved at a time
+stacks = [x[:] for x in initial_stacks]
 for line in movement_data:
     num_crates, start, end = [int(x) for x in re.sub('\D', ' ', line).split()]
     for _ in range(num_crates):
         crate = stacks[start-1].pop()
         stacks[end-1].append(crate)
+print(f"Part 1 - {''.join(stack[-1] for stack in stacks)}")
 
-top_seqeuence = ''
-for stack in stacks:
-    top_seqeuence += stack[-1]
-print(top_seqeuence)
+# Part 2 - CrateMover 9001 - multiple crates moved at a time
+stacks = [x[:] for x in initial_stacks]
+for line in movement_data:
+    num_crates, start, end = [int(x) for x in re.sub('\D', ' ', line).split()]
+    start_stack = stacks[start-1]
+    split_point = len(start_stack) - num_crates
+    stacks[start-1], crates_to_move = start_stack[:split_point], start_stack[split_point:]
+    stacks[end-1].extend(crates_to_move)
+print(f"Part 2 - {''.join(stack[-1] for stack in stacks)}")
