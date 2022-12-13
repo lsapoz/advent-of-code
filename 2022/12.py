8f2780bac6da464d7a6c5711d2b1e4c07f9b3b48
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2022-12-12 18:43:51 -0800

    Day 12 Part 2

--- 2022/12.py
+++ 2022/12.py
@@ -7,30 +7,27 @@ lines = Path(here/"12.txt").read_text().splitlines()
 height_map = [[x for x in line] for line in lines]
 M, N = len(height_map), len(height_map[0])
 
-S, E = None, None
-for i in range(M):
-    for j in range(N):
-        if height_map[i][j] == 'S':
-            S = (i, j)
-            height_map[i][j] = 'a'
-        elif height_map[i][j] == 'E':
-            E = (i, j)
-            height_map[i][j] = 'z'
-        height_map[i][j] = ord(height_map[i][j]) - ord('a')
-
-def get_shortest_path():
-    q = deque([(S[0], S[1], 0)])
+def shortest_descent(end: str, start: str):
+    def valid_move(x, y, nx, ny) -> bool:
+        if nx < 0 or nx >= M or ny < 0 or ny >= N or (nx, ny) in visited:
+            return False
+        a = height_map[x][y].replace('S', 'a').replace('E', 'z')
+        b = height_map[nx][ny].replace('S', 'a').replace('E', 'z')
+        return ord(a) - ord(b) <= 1
+    
+    q = deque([(i, j, 0) for i in range(M) for j in range(N) if height_map[i][j] == end])
     visited = set()
     while q:
         x, y, steps = q.popleft()
 
-        if (x, y) == E:
+        if height_map[x][y] == start:
             return steps
 
         for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             nx, ny = x + dx, y + dy
-            if nx >= 0 and nx < M and ny >= 0 and ny < N and (nx, ny) not in visited and height_map[nx][ny] - height_map[x][y] <= 1:
+            if valid_move(x, y, nx, ny):
                 visited.add((nx, ny))
                 q.append((nx, ny, steps + 1))
 
-print(f"Part 1: {get_shortest_path()}")
+print(f"Part 1: {shortest_descent('E', 'S')}")
+print(f"Part 2: {shortest_descent('E', 'a')}")
