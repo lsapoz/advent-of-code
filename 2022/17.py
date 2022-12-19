0b0fa0168280da2a90a0ce2085845dd4f91427fa
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2022-12-19 08:58:14 -0800

    Day 17 Part 2

--- 2022/17.py
+++ 2022/17.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from collections import namedtuple
-from typing import  Set
+from typing import Dict, List, Optional, Set, Tuple
 
 here = Path(__file__).parent
 jets = Path(here/"17.txt").read_text()
@@ -29,9 +29,11 @@ def spawn(shape: str, top: int) -> Set[Point]:
 
 
 def simulate(num_rocks: int) -> int:
-    grid: Set[Point] = set()
+    grid: Set[Point] = set()  # set of all points representing stopped rocks
+    height_deltas: List[int] = []  # list containing the change in height of the tower after every rock stops falling
+    keys: Dict[Tuple, int] = {} # dict that maps a key of (shape_idx, jet_idx, height_delta) to the rock_idx
 
-    def move(piece: Set[Point], offset: Point):
+    def move(piece: Set[Point], offset: Point) -> Set[Point]:
         moved_piece: Set[Point] = set()
         for point in piece:
             new_point = Point(point.x + offset.x, point.y + offset.y)
@@ -40,26 +42,61 @@ def simulate(num_rocks: int) -> int:
             moved_piece.add(new_point)
         return moved_piece
 
-    def push(piece: Set[Point], jet: str):
+    def push(piece: Set[Point], jet: str) -> Set[Point]:
         return move(piece, Point(1 if jet == '>' else -1, 0))
 
-    def drop(piece: Set[Point]):
+    def drop(piece: Set[Point]) -> Set[Point]:
         return move(piece, Point(0, -1))
 
-    jet, top = -1, -1
-    for i in range(num_rocks):
-        piece = spawn(SHAPES[i % len(SHAPES)], top)
+    # track patterns in height diff
+    # once a cycle is detected, return the # of rocks in each cycle
+    def track_cycle(rock_idx: int, shape_idx: int, jet_idx: int, height_delta: int) -> Optional[int]:
+        height_deltas.append(height_delta)
+        key = (shape_idx, jet_idx, height_delta)
+        if key in keys:
+            cycle_length = rock_idx - keys[key]
+
+            # check that the last cycle_length height deltas match the previous cycles exactly
+            if len(height_deltas) > cycle_length * 2 and height_deltas[-cycle_length:] == height_deltas[-cycle_length*2:-cycle_length]:
+                return cycle_length
+
+        keys[key] = rock_idx
+        return None
+
+    rock_idx = 0
+    jet_idx, top = -1, -1
+    cycle_length = None
+    while rock_idx < num_rocks:
+        shape_idx = rock_idx % len(SHAPES)
+        piece = spawn(SHAPES[shape_idx], top)
         while True:
-            jet = (jet + 1) % len(jets)
-            piece = push(piece, jets[jet])
+            jet_idx = (jet_idx + 1) % len(jets)
+            piece = push(piece, jets[jet_idx])
 
             dropped_piece = drop(piece)
             if dropped_piece == piece:
                 break
             piece = dropped_piece
-        top = max(top, *[point.y for point in piece])
+        height_delta = max(top, *[point.y for point in piece]) - top
+        top += height_delta
         grid.update(piece)
-    return top + 1
+
+        cycle_length = track_cycle(rock_idx, shape_idx, jet_idx, height_delta)
+        rock_idx += 1
+        
+        if cycle_length:
+            break
+
+    current_height = top + 1
+    if not cycle_length:
+        return current_height
+
+    cycles, remainder = divmod(num_rocks - rock_idx, cycle_length)
+    cycle_height = sum(height_deltas[-cycle_length:])
+    remainder_height = sum(height_deltas[-cycle_length:-cycle_length+remainder])
+
+    return current_height + (cycles * cycle_height) + remainder_height
 
 
 print(f"Part 1: {simulate(2022)}")
+print(f"Part 2: {simulate(1000000000000)}")
