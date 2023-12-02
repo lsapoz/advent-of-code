d99a79671160cd40947a170398fa78bd989e3773
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2023-12-01 18:29:43 -0800

    Day 1 Part 2

--- 2023/01.py
+++ 2023/01.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 lines = Path(__file__).resolve().with_suffix(".txt").read_text().splitlines()
 
@@ -7,4 +8,23 @@ for line in lines:
     first = next(x for x in line if x.isnumeric())
     last = next(x for x in reversed(line) if x.isnumeric())
     total += int(first + last)
-print(total)
+print(f"Part 1: {total}")
+
+mapping = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+reversed_mapping = {k[::-1]: v for k, v in mapping.items()}
+total = 0
+for line in lines:
+    first = re.search(f"\d|{'|'.join(mapping.keys())}", line).group(0)
+    last = re.search(f"\d|{'|'.join(reversed_mapping.keys())}", line[::-1]).group(0)
+    total += int(mapping.get(first, first) + reversed_mapping.get(last, last))
+print(f"Part 2: {total}")
