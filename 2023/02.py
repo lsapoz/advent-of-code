f466ea8f2ba5ebf2668ff4c0002df823fff4ad9d
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2023-12-02 09:03:09 -0800

    Day 2 Part 2

--- 2023/02.py
+++ 2023/02.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import math
 
 lines = Path(__file__).resolve().with_suffix(".txt").read_text().splitlines()
 
@@ -15,9 +16,23 @@ def is_game_possible(game_sets: list[str]):
     return True
 
 
+def get_game_power(game_sets: list[str]):
+    min_cubes = {"red": 0, "green": 0, "blue": 0}
+    for s in game_sets:
+        cubes = s.split(",")
+        for cube in cubes:
+            num, color = cube.strip().split(" ")
+            min_cubes[color] = max(min_cubes[color], int(num))
+    return math.prod(min_cubes.values())
+
+
 ids_sum = 0
+powers_sum = 0
 for idx, line in enumerate(lines):
     game_no, results = line.split(":")
-    if is_game_possible(results.split(";")):
+    sets = results.split(";")
+    if is_game_possible(sets):
         ids_sum += idx + 1
-print(ids_sum)
+    powers_sum += get_game_power(sets)
+print(f"Part 1: {ids_sum}")
+print(f"Part 2: {powers_sum}")
