9957b8a18264d1f3c9a0806abca66cb81b008c49
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2023-12-04 08:45:47 -0800

    Day 4 Part 2

--- 2023/04.py
+++ 2023/04.py
@@ -1,12 +1,15 @@
 from pathlib import Path
-import re
 
 lines = Path(__file__).resolve().with_suffix(".txt").read_text().splitlines()
 
-total = 0
-for line in lines:
+points = [0] * len(lines)
+copies = [1] * len(lines)
+for idx, line in enumerate(lines):
     _, all_nums = line.split(":")
     winning_nums, my_nums = [[int(num) for num in nums.split(" ") if num.isnumeric()] for nums in all_nums.split("|")]
     overlap = set(winning_nums).intersection(set(my_nums))
-    total += 2 ** (len(overlap) - 1) if len(overlap) > 0 else 0
-print(total)
+    points[idx] = 2 ** (len(overlap) - 1) if len(overlap) > 0 else 0
+    for i in range(idx + 1, min(idx + len(overlap) + 1, len(lines))):
+        copies[i] += copies[idx]
+print(f"Part 1: {sum(points)}")
+print(f"Part 2: {sum(copies)}")
