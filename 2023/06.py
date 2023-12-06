9020f7f181d304a32eea95774a57c0e3c615d0a0
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2023-12-05 21:37:11 -0800

    Day 6 Part 2

--- 2023/06.py
+++ 2023/06.py
@@ -4,11 +4,22 @@ lines = Path(__file__).resolve().with_suffix(".txt").read_text().splitlines()
 times = [int(x) for x in lines[0][9:].split()]
 distances = [int(x) for x in lines[1][9:].split()]
 
-total = 1
-for time, distance in zip(times, distances):
+
+def get_ways_to_win(t: int, d: int):
     ways = 0
     for i in range(1, time - 1):
         if i * (time - i) > distance:
             ways += 1
-    total *= ways
-print(total)
+        elif ways > 0:
+            break
+    return ways
+
+
+total = 1
+for time, distance in zip(times, distances):
+    total *= get_ways_to_win(time, distance)
+print(f"Part 1: {total}")
+
+time = int(lines[0][9:].replace(" ", ""))
+distance = int(lines[1][9:].replace(" ", ""))
+print(f"Part 2: {get_ways_to_win(time, distance)}")
