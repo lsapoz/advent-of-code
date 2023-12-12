af2e8b48310ae28dca9799f99c24776d056beae2
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2023-12-11 18:26:06 -0800

    Day 11 Part 2

--- 2023/11.py
+++ 2023/11.py
@@ -14,16 +14,18 @@ for i, line in enumerate(lines):
 empty_rows = [idx for idx, is_empty in enumerate(empty_rows) if is_empty]
 empty_cols = [idx for idx, is_empty in enumerate(empty_cols) if is_empty]
 
-total = 0
+distances = []
 for i in range(len(galaxies)):
     for j in range(i + 1, len(galaxies)):
         (x1, y1), (x2, y2) = galaxies[i], galaxies[j]
         dist = abs(x1 - x2) + abs(y1 - y2)
+        extra = 0
         for empty_row in empty_rows:
             if empty_row in range(min(x1, x2), max(x1, x2)):
-                dist += 1
+                extra += 1
         for empty_col in empty_cols:
             if empty_col in range(min(y1, y2), max(y1, y2)):
-                dist += 1
-        total += dist
-print(total)
+                extra += 1
+        distances.append((dist, extra))
+print(f"Part 1: {sum(dist + extra for dist, extra in distances)}")
+print(f"Part 2: {sum(dist + (extra * (1000000 - 1)) for dist, extra in distances)}")
