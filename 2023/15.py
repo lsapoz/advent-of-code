18217a6fed6b941243faef4b052af454de6637cd
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2023-12-14 22:22:01 -0800

    Day 15 Part 2

--- 2023/15.py
+++ 2023/15.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from collections import defaultdict
 
 steps = Path(__file__).resolve().with_suffix(".txt").read_text().replace("\n", "").split(",")
 
@@ -12,4 +13,22 @@ def HASH(s: str):
     return val
 
 
-print(sum(HASH(step) for step in steps))
+print(f"Part 1: {sum(HASH(step) for step in steps)}")
+
+boxes: dict[int, dict[str, int]] = defaultdict(dict)
+for step in steps:
+    op = "=" if "=" in step else "-"
+    focal_length = int(step[-1]) if op == "=" else None
+    label = step[:-2] if op == "=" else step[:-1]
+
+    box_no = HASH(label)
+    if op == "=":
+        boxes[box_no][label] = focal_length
+    else:
+        boxes[box_no].pop(label, None)
+
+total = 0
+for box_no, lenses in boxes.items():
+    for idx, focal_length in enumerate(lenses.values()):
+        total += (box_no + 1) * (idx + 1) * focal_length
+print(f"Part 2: {total}")
