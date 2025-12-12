cd289d80d0b22ea935726ba91344f31717dbddaf
Lyndon Sapozhnik <lyndonsapozhnik@gmail.com>
2025-12-11 18:40:34 -0800

    Day 11 Part 2

--- 2025/11.py
+++ 2025/11.py
@@ -1,20 +1,26 @@
 from pathlib import Path
+from functools import cache
 
 lines = Path(__file__).resolve().with_suffix(".txt").read_text().splitlines()
 devices = {line.split(":")[0]: line.split(":")[1].split() for line in lines}
-START, END = "you", "out"
 
 
-def dfs(device=START):
+@cache
+def dfs(
+    device: str,
+    end: str,
+    required: frozenset[str] = frozenset(),
+):
     paths = 0
 
-    if device == END:
-        return 1
+    if device == end:
+        return 1 if not required else 0
 
     for output in devices[device]:
-        paths += dfs(output)
+        paths += dfs(output, end, required - {device})
 
     return paths
 
 
-print(f"Part 1: {dfs()}")
+print(f"Part 1: {dfs("you", "out")}")
+print(f"Part 2: {dfs("svr", "out", frozenset(["dac", "fft"]))}")
